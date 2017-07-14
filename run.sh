edacb4fcff05a90a4d125427f6ea7a43562e0421
Tom Berghuis <thomas.berghuis@gmail.com>
2017-07-14 14:56:00 +1000

    run for theRealElmo

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ node index.js bigbird88 60b725f10c9c85c70d97880dfe8191b3 |& tee -a output.txt
 
 echo | tee -a output.txt
 echo '$ node index.js theRealElmo' | tee -a output.txt
-node index.js theRealElmo 60b725f10c9c85c70d97880dfe8191b3 |& tee -a output.txt
+node index.js theRealElmo |& tee -a output.txt
