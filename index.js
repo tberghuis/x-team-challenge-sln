21eabec951ba77201f9f2dfb2bbea1d3df633cb8
Tom Berghuis <thomas.berghuis@gmail.com>
2017-07-14 14:12:39 +1000

    fixed encode

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-var path = require('path')
-var funcs = require('./funcs')
-var encodeName = funcs.encodName
+var path = require("path");
+var funcs = require("./funcs");
+var encodeName = funcs.encodeName;
 
 var session = {
   username: process.argv[2],
   lastMessageHash: process.argv[3]
-}
+};
 
 if (!session.username || !session.lastMessageHash) {
-  console.log('Usage: node index.js <username> <hash>')
-  process.exit(0)
+  console.log("Usage: node index.js <username> <hash>");
+  process.exit(0);
 }
 
 // 1. load the database
-var dbFile = path.join(__dirname, 'db', 'index.json')
-funcs.loadDb(dbFile, function (err, db) {
-
+var dbFile = path.join(__dirname, "db", "index.json");
+funcs.loadDb(dbFile, function(err, db) {
   // 2. encode the name
-  var encoded = encodeName(session.usrename)
+  var encoded = encodeName(session.usrename);
 
   // 3. find the user's inbox
-  var inbox = funcs.findInbox(db, encoded)
+  var inbox = funcs.findInbox(db, encoded);
 
   // 4. find the next message
-  var nextMessage = funcs.findNextMessage(inbox, session.lastMessageHash)
+  var nextMessage = funcs.findNextMessage(inbox, session.lastMessageHash);
 
   // 5. print out the message.
   // Paste the console output into the "Solution" field and you're done!
-  console.log(nextMessage)
-})
+  console.log(nextMessage);
+});
