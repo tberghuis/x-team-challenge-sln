35a793465b38e9b2426b97733344507d2344c592
Tom Berghuis <thomas.berghuis@gmail.com>
2017-07-14 15:00:40 +1000

    run for alice

--- run.sh
+++ run.sh
@@ -7,3 +7,8 @@ node index.js bigbird88 60b725f10c9c85c70d97880dfe8191b3 |& tee -a output.txt
 echo | tee -a output.txt
 echo '$ node index.js theRealElmo' | tee -a output.txt
 node index.js theRealElmo |& tee -a output.txt
+
+echo | tee -a output.txt
+echo '$ node index.js alice 3b5d5c3712955042212316173ccf37be' | tee -a output.txt
+node index.js alice 3b5d5c3712955042212316173ccf37be |& tee -a output.txt
+
